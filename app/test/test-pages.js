3e97af2815db8295e2720d304c0db715f4cb437f
Pascal Lauener <pascal@lauener.email>
2018-03-31 21:17:24 +0200

    update

--- app/test/test-pages.js
+++ app/test/test-pages.js
@@ -2,7 +2,7 @@ var expect  = require('chai').expect;
 var request = require('request');
 
 it('Main page content', function(done) {
-    request('http://localhost:9000' , function(error, response, body) {
+    request('http://localhost:80' , function(error, response, body) {
         expect(body).to.equal('Hello World');
         done();
     });
